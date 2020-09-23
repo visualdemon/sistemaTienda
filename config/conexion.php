8c5913eda4027dc0f9e96b68b0d69470781bed36
Wilber Jurado <wilberamp@gmail.com>
2020-09-22 22:17:39 -0500

    Agregado lineas al conexion

--- config/conexion.php
+++ config/conexion.php
@@ -9,3 +9,36 @@ if (mysqli_connect_errno())
     printf("Fallo conexion a la base de datos: %s\n", mysqli_connect_errno());
     exit();
 }
+
+if (!function_exists('ejecutarConsulta')) {
+
+    function ejecutarConsulta($sql) {
+
+        global $conexion;
+        $query = $conexion -> query($sql);
+        return $query;
+    }
+
+    function ejecutarConsultaSimpleFila($sql) {
+
+        global $conexion;
+        $query = $conexion -> query($sql);
+        $row = $query -> fetch_assoc();
+        return $row;
+
+    }
+
+    function ejecutarConsulta_retornarID($sql) {
+
+        global $conexion;
+        $query = $conexion -> query($sql);
+        return $conexion -> insert_id;
+    }
+
+    function limpiarCadena($sql) {
+
+        global $conexion;
+        $str = mysqli_real_escape_string($conexion,trim($str));
+        return htmlspecialchars($str);
+    }
+}
